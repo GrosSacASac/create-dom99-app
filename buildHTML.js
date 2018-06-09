b855c9b8ed0b365d6a0c2a6c529725240a5ba7e6
Cyril Walle <capocyril@hotmail.com>
2018-06-09 11:11:35 +0200

    recusively import html files

--- buildHTML.js
+++ buildHTML.js
@@ -19,22 +19,20 @@ const thisName = "html build";
 const INDEX_PATH = "index.html";
 const BUILT_PATH = "final.html";
 const devLoaderString = `<script type="module" src="devLoader.js"></script>`;
+// todo use this map
+const map = {};
 
-Promise.all([
-    textFileContentPromiseFromPath(INDEX_PATH),
-]).then(function ([indexHTMLString]) {
-    let newIndex = indexHTMLString;
+const inlineHTML = function (html) {
+    let newHTML = html;
+    const findInlines = /<script type="text\/html" data-inline src="([^"]+)"><\/script>/g;
     
-    const regularExpression = /<script type="text\/html" data-inline src="([^"]+)"><\/script>/g;
-    
-    const allMatches = []
-    let matches = true;
-    while(matches = regularExpression.exec(indexHTMLString)) {
+    const allMatches = [];
+    let matches;
+    while(matches = findInlines.exec(newHTML)) {
         allMatches.push(matches);
-        
     }
-    if (!allMatches.length) {
-        return;
+    if (allMatches.length === 0) {
+        return Promise.resolve(newHTML);
     }
     
     return Promise.all(
@@ -42,25 +40,33 @@ Promise.all([
             return textFileContentPromiseFromPath(match[1]);
         })
     ).then(function (importedHTMLs) {
+        return Promise.all(
+            importedHTMLs.map(function (importedHTML) {
+                return inlineHTML(importedHTML);
+            })
+        );
+    }).then(function (importedHTMLs) {
         importedHTMLs.forEach(function (importedHTML, i) {
-            newIndex = newIndex.replace(allMatches[i][0], importedHTML);
+            newHTML = newHTML.replace(allMatches[i][0], importedHTML);
+        
         });
     }).catch(function (error) {
         console.log(error);
         console.log(`
         Could not import file ${error.path}`);
     }).then(function () {
-        if (newIndex === indexHTMLString) {
-            //console.log(indexHTMLString);
-            return;
-        }
-        newIndex = newIndex.replace(devLoaderString, ``);
-        return writeTextInFilePromiseFromPathAndString(BUILT_PATH, newIndex);    
+        return newHTML;            
     });
-    
-}).then(function () {
-    
+};
 
+Promise.all([
+    textFileContentPromiseFromPath(INDEX_PATH),
+]).then(function ([indexHTMLString]) {
+    const withOutDevloader = indexHTMLString.replace(devLoaderString, ``);;    
+    return inlineHTML(withOutDevloader);
+    
+}).then(function (newHTML) {    
+    return writeTextInFilePromiseFromPathAndString(BUILT_PATH, newHTML);
 }).catch(function (reason) {
     const errorText = thisName + " failed: " + String(reason);
     console.log(errorText);
--- devLoader.js
+++ devLoader.js
@@ -5,47 +5,47 @@ replaceWith polyfill
 https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/replaceWith
 does ${script.src} need to ne html escaped ? 
 */
-// convert to a frozen array
-const scripts = [...document.getElementsByTagName(`script`)];
+const map = {};
+const importHTML = function (startNode) {
+  // convert to a frozen array
+  const scripts = [...startNode.querySelectorAll(`script`)];
 
-scripts.forEach(function (script) {
-  if (!script.hasAttribute(`data-inline`)) {
-    return;
-  }
-  const request = new XMLHttpRequest();
-  request.overrideMimeType("text/plain");
-  request.open('GET', script.src, false);
-  try {
-    request.send(null);
-  } catch (error) {
-    console.log(error, `problem loading ${script.src}`);
-  }
-  
-  let htmlString;
-  if (request.status === 200) {
-    htmlString = request.responseText;
-  } else {
-    htmlString = `<strong>problem loading ${script.src}</strong>`;
-  }
-  
-  // a way to parse a html string and add it inline in the DOM
-  // without container element like <div></div>
-  const template = document.createElement('template');
-  template.innerHTML = htmlString;
-  
-  script.replaceWith(document.importNode(template.content, true));
- 
-  
+  scripts.forEach(function (script) {
+    if (!script.hasAttribute(`data-inline`)) {
+      return;
+    }
+    let clone;
+    if (Object.prototype.hasOwnProperty.call(map, script.src)) {
+      clone = map[script.src].cloneNode(true);
+    } else {
+      const request = new XMLHttpRequest();
+      request.overrideMimeType("text/plain");
+      request.open('GET', script.src, false);
+      try {
+        request.send(null);
+      } catch (error) {
+        console.log(error, `problem loading ${script.src}`);
+      }
+      
+      let htmlString;
+      if (request.status === 200) {
+        htmlString = request.responseText;
+      } else {
+        htmlString = `<strong>problem loading ${script.src}</strong>`;
+      }
+      
+      // a way to parse a html string and add it inline in the DOM
+      // without container element like <div></div>
+      const template = document.createElement('template');
+      template.innerHTML = htmlString;
+      clone = document.importNode(template.content, true);
+      importHTML(clone);
+      map[script.src] = clone.cloneNode(true);
+    }
+    
+    script.replaceWith(clone);
 
-});
+  });
+};
 
-//fetch(script.src).
-//then(function(response) {
-  //return response.text();
-//}).then(function (htmlString) {
-  //const template = document.createElement('template');
-  //template.innerHTML = htmlString;
-  //
-  //script.replaceWith(document.importNode(template.content, true));
-//});
-//}
+importHTML(document);
