48294f70d49f021474e985adddd2cf664ede7e90
Cyril Walle <capocyril@hotmail.com>
2018-07-03 23:20:10 +0200

    working on new plugin

---
+++ tests/plugin-cloned/main.js
@@ -0,0 +1,16 @@
+//main.js
+import {d} from "../../../node_modules/dom99/source/dom99.js";
+
+d.start();
+
+d.feed({users :
+    [
+        {
+            picture: "boss.jpg",
+            bio: "Loves biking and skating"
+        },
+        {
+            picture: "sister.jpg",
+            bio: "Drinks tons of café."
+        }
+]});
