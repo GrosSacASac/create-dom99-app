f13532f9e0524f9c151ec4bb19ed8a8f1184c2e5
Walle Cyril <cyril.walle@protonmail.com>
2018-09-30 20:50:58 +0200

    fix imports

--- tests/html-include/html-only/main.js
+++ tests/html-include/html-only/main.js
@@ -3,6 +3,6 @@ import * as d from "../../../node_modules/dom99/built/dom99ES.js";
 
 d.start();
 d.feed({
-  "p": "targets paragraph.html",
+  "p": `targets paragraph.html via data-variable="p"`,
   "i": "targets index.html"
 });
