3c6eae87a861e3e73d5d3937c3f19525f32c9b92
Walle Cyril <cyril.walle@protonmail.com>
2019-12-17 21:14:27 +0100

    update

--- source/js/helpTexts.js
+++ source/js/helpTexts.js
@@ -1,10 +1,11 @@
-export { start, stop };
+export { start };
 import * as d from "./dependencies.js";
-
+import { helpText } from "./settings.js";
 
 const showHelp = function () {
     d.elements.showHelp.remove();
     d.feed(`helpText`, helpText);
+    console.log("hi");
 };
 
 const start = function () {
--- source/js/main.js
+++ source/js/main.js
@@ -13,20 +13,20 @@ core.on(ALL, ({ name, data, time }) => {
     const timeString = new Date(time).toISOString();
     console.debug(`${timeString} event ${String(name)} with data`, data);
 });
+(async () => {
+    await core.start(helpTexts);
+    await core.start(xClock);
 
-core.start(helpTexts);
-core.start(xClock);
+    // here executes before dom99 went through
+    // here you cannot use d.elements
+    d.start({
+        initialFeed: {
+            title: `Hello World`,
+            superParagraph: `Super Paragraph text`,
+        },
+    });
 
-// here executes before dom99 went through
-// here you cannot use d.elements
-
-d.start({
-    initialFeed: {
-        title: `Hello World`,
-        superParagraph: `Super Paragraph text`,
-    },
-});
-
-// executes after dom99 went through
-// here you can use d.elements
-d.elements.loadingHint.remove();
+    // executes after dom99 went through
+    // here you can use d.elements
+    d.elements.loadingHint.remove();
+})();
