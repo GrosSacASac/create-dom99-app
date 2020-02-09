4fc7574e1a201ade031cd83f0c530caff3718e67
Walle Cyril <cyril.walle@protonmail.com>
2020-02-09 22:59:37 +0100

    move to html-sac

--- tests/html-include/html-css/main.js
+++
@@ -1,8 +0,0 @@
-//main.js
-import * as d from "../../../node_modules/dom99/source/dom99.js";
-
-d.start();
-d.feed({
-    "p": `targets paragraph.html`,
-    "i": `targets index.html`,
-});
--- tests/html-include/html-only/main.js
+++
@@ -1,8 +0,0 @@
-//main.js
-import * as d from "../../../node_modules/dom99/source/dom99.js";
-
-d.start();
-d.feed({
-    "p": `targets paragraph.html via data-variable="p"`,
-    "i": `targets index.html`,
-});
