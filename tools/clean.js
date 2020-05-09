7d25052c13c6254e917095a0521b3af5a0d89952
Cyril Walle <cyril.walle@protonmail.com>
2020-05-10 01:29:39 +0200

    fixes

--- tools/clean.js
+++ tools/clean.js
@@ -3,5 +3,5 @@ import { deleteFile } from "filesac";
 import fs from "fs";
 
 
-fs.rename(`dist/index-temp.html`, `dist/index.html`);
+fs.rename(`dist/index-temp.html`, `dist/index.html`, function() {});
 deleteFile(`source/index-temp.html`);
